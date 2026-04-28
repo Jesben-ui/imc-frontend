80f216094b55ae64e5529f10d5d0c493855dc430
Jesben-ui <mbahjesben@gmail.com>
2026-04-28 15:16:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -339,7 +339,7 @@ const regressionData = {
   <p>
     Équation de la droite de Régression linéaire:
     <strong>
-      x = {y-(b.toFixed(2))} + {a.toFixed(2)}
+      x = {taille - (b.toFixed(2))} + {a.toFixed(2)}
     </strong>
   </p>
 </div>
